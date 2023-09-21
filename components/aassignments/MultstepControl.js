41725e4d1855ba5fa6e01fd2a4ffbae81de6856c
aseer-gov-sa <144770482+omarmoha23@users.noreply.github.com>
2023-09-21 13:19:53 +0300

    v4

--- components/aassignments/MultstepControl.js
+++ components/aassignments/MultstepControl.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const MultstepControl = () => {
+const MultstepControl = ({ handleClick, currentStep, steps }) => {
   return (
-    <div className=' container flex justify-around mt-4 mb-8'>
-        <button className=' bg-white text-salte-400 uppercase py-2 px-4 rounded-xl cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transtion duration-200'>
-            Back
-        </button>
-        <button className=' bg-white text-salte-400 uppercase py-2 px-4 rounded-xl cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transtion duration-200'>
-            Next
-        </button>
-
-
+    <div className='container flex justify-around mt-4 mb-8'>
+      <button
+        onClick={() => handleClick()}
+        className={`bg-white text-slate-400 uppercase py-2 px-4 rounded-xl cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transtion duration-200 ease-in-out
+         ${currentStep === 1 ? 'opacity-50 cursor-not-allowed' : ''}`}
+      >
+        Back
+      </button>
+      <button
+        onClick={() => handleClick('next')}
+        className='bg-green-500 text-white uppercase py-2 px-4 font-semibold rounded-xl cursor-pointer  hover:bg-slate-700 hover:text-white transtion duration-200 ease-in-out'
+      >
+        {currentStep === steps.length - 1 ? 'Confirm' : 'Next'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default MultstepControl
+export default MultstepControl;
